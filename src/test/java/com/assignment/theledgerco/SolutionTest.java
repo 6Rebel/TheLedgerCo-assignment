00b3e0e26879bb500bf38a5689c559b51a0de84a
Harish Rana <harishrana1336@gmail.com>
2022-04-23 17:41:10 +0530

    assignment problem

---
+++ src/test/java/com/assignment/theledgerco/SolutionTest.java
@@ -0,0 +1,22 @@
+package com.assignment.theledgerco;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private Solution solutionUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        solutionUnderTest = new Solution();
+    }
+
+    @Test
+    void testMain() {
+
+        // Run the test
+        Solution.main(new String[]{"value"});
+
+    }
+}
